e021ee74a0da5b0f4f58dcbe6226362cc050e147
Thắng Trương <haupro186@gmail.com>
2021-04-24 12:48:24 +0700

    final of final :))

--- src/toDoItems.js
+++ src/toDoItems.js
@@ -3,7 +3,11 @@ import "./toDoItem.css";
 export default function ToDoItem({ index, item, onClickCheck, removeItem }) {
   return (
     <div id="toDoItems">
-      <input type="checkbox" onClick={onClickCheck} />
+      <input
+        type="checkbox"
+        onClick={onClickCheck}
+        checked={item.isCompleted}
+      />
       <h3 style={{ margin: "0" }}>{index + 1}</h3>
       <h3 className="title">
         {item.title} ({item.isCompleted ? "Completed" : "Not Completed"})
